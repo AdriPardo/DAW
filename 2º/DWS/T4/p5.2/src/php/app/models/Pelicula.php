6f6aaaa78dd163434aa339f8c7da12a7e73ec147
Adrian Pardo <adripardo72@gmail.com>
2021-10-29 12:35:02 +0200

    Subo el curso

---
+++ 2º/DWS/T4/p5.2/src/php/app/models/Pelicula.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\models;
+
+use core\Model;
+
+class Pelicula extends Model
+{
+    protected $table = 'peliculas';
+
+    protected function getActores($id)
+    {
+
+        return $this->belongsToMany('actores', 'pelicula_actor', 'id_pelicula', 'id_actor', $id);
+    }
+
+    protected function getDirectores($id)
+    {
+
+        return $this->belongsToMany('directores', 'pelicula_director', 'id_pelicula', 'id_director', $id);
+    }
+}
